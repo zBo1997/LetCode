6f4c3df13f2ba0c715a1e3c4044d9f0085e20a4d
zbo1997 <605345115@qq.com>
2021-09-03 00:08:30 +0800

    SuccessOrNode

---
+++ src/Day9/SuccessOrNode.java
@@ -0,0 +1,62 @@
+package Day9;
+
+
+/**
+ * @Classname SuccessOrNode
+ * @Description
+ * @Date 2021/9/2 23:01
+ * @Created by ZhuBo
+ */
+public class SuccessOrNode {
+
+    public static class Node {
+        public int value;
+
+        public Node left;
+
+        public Node right;
+
+        public Node parent;
+
+        public Node(int data) {
+            this.value = data;
+        }
+    }
+
+    /**
+     * （中序遍历的前提下） 指定节点node 寻找次二叉数的 下一节 时间复杂度位 O(K) K位node的深度
+     * @param node
+     * @return
+     */
+    public static Node getSuccessNode(Node node) {
+        if (node == null) {
+            return null;
+        }
+        if (node.right != null) {
+            return getLeftMost(node);
+        } else {//当前节点没有右子树（就说明 我们寻找的后继节点 需要找父节点）
+            Node parent = node.parent;//
+            //当前节点有父节点，并且当前节点不是父节点的左孩子
+            while (parent != null && parent.left != node) {
+                node = parent;//当前节点上移动
+                parent = node.parent;//当前节点的父节点
+            }
+            return parent;
+        }
+    }
+
+    /**
+     * 始终遍历到最左节点
+     * @param node
+     * @return
+     */
+    private static Node getLeftMost(Node node) {
+        if (node == null) {
+            return null;
+        }
+        while (node.left != null) {
+            node = node.left;
+        }
+        return node;
+    }
+}
