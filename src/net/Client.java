5ac78d3892cbe114d1a393c3fb7659d64b50a96c
zBo1997 <605345115@qq.com>
2024-05-25 22:54:59 +0800

    提交告警

--- src/net/Client.java
+++ src/net/Client.java
@@ -9,23 +9,37 @@ import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
+        Scanner scanner = null;
+        Socket socket = null;
         try {
-            Socket socket = new Socket("localhost", 8000);
+            socket = new Socket("localhost", 8000);
             while (true) {
                 // 从控制台读取输入
-                Scanner scanner = new Scanner(System.in);
+                scanner = new Scanner(System.in);
                 System.out.println("请输入：");
                 String input = scanner.nextLine();
                 OutputStream outputStream = socket.getOutputStream();
                 outputStream.write(input.getBytes(StandardCharsets.UTF_8));
                 InputStream inputStream = socket.getInputStream();
-                //读取这个流并且输出到控制台
+                // 读取这个流并且输出到控制台
                 byte[] data = new byte[1024];
                 int read = inputStream.read(data);
                 System.out.println(new String(data, 0, read, StandardCharsets.UTF_8));
             }
         } catch (IOException e) {
             System.out.println("Failed to connect: " + e.getMessage());
+        } finally {
+            if (scanner != null) {
+                scanner.close();
+            }
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
