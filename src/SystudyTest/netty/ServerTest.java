911e461be94256a2e59ef4a69227a144cd23138d
zBo1997 <605345115@qq.com>
2024-08-03 18:50:22 +0800

    Reapply "编码器学习"
    This reverts commit fc59a83eb0ad6c2ac86ae4de2680801240b7a529.

--- src/SystudyTest/netty/ServerTest.java
+++ src/SystudyTest/netty/ServerTest.java
@@ -1,25 +1,23 @@
 package SystudyTest.netty;
 
+import static io.netty.channel.ChannelFutureListener.FIRE_EXCEPTION_ON_FAILURE;
+
+import java.util.List;
+
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelDuplexHandler;
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.ChannelPipeline;
+import io.netty.channel.DefaultEventLoopGroup;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.handler.codec.string.StringDecoder;
-import io.netty.handler.codec.string.StringEncoder;
-import io.netty.handler.timeout.IdleState;
-import io.netty.handler.timeout.IdleStateEvent;
-import io.netty.handler.timeout.IdleStateHandler;
-import io.netty.util.CharsetUtil;
-import io.netty.channel.ChannelHandler.Sharable;
-
-import static io.netty.channel.ChannelFutureListener.FIRE_EXCEPTION_ON_FAILURE;
+import io.netty.handler.codec.ByteToMessageDecoder;
 
 /**
  * @Date 2024/7/28
@@ -31,33 +29,35 @@ public class ServerTest {
         EventLoopGroup group = new NioEventLoopGroup();
         EventLoopGroup work = new NioEventLoopGroup();
         ServerBootstrap serverBootstrap = new ServerBootstrap();
+        DefaultEventLoopGroup defaultEventLoopGroup = new DefaultEventLoopGroup();
         serverBootstrap.group(group, work).channel(NioServerSocketChannel.class)
-            .option(ChannelOption.SO_BACKLOG, 128).option(ChannelOption.SO_REUSEADDR, true)
-            .childHandler(new ChannelInitializer<SocketChannel>() {
+                .option(ChannelOption.SO_BACKLOG, 128).option(ChannelOption.SO_REUSEADDR, true)
+                .childHandler(new ChannelInitializer<SocketChannel>() {
 
-                /**
-                 * 实现自定义初始化的Channel的逻辑
-                 */
-                @Override
-                protected void initChannel(SocketChannel ch) throws Exception {
-                    ChannelPipeline pipeline = ch.pipeline();
-
-                    // 添加字符串解码器和编码器
-                    pipeline.addLast(new StringDecoder(CharsetUtil.UTF_8));
-                    pipeline.addLast(new StringEncoder(CharsetUtil.UTF_8));
-
-                    // 心跳检测
-                    pipeline.addFirst("idleStateHandler", new IdleStateHandler(0, 10, 0));
-                    // 超过心跳的处理
-                    pipeline.addAfter("idleStateHandler", "idleEventHandler", new IdleTimeoutHandler());
-                }
-
-            })
-            // 设置channel的参数
-            .childOption(ChannelOption.SO_KEEPALIVE, true).childOption(ChannelOption.TCP_NODELAY, true);
+                    /**
+                     * 实现自定义初始化的Channel的逻辑
+                     */
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        ChannelPipeline pipeline = ch.pipeline();
+                        pipeline.addLast(new CustomChannelDeocder());
+                        pipeline.addLast(new ChannelInboundHandlerAdapter() {
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                if (msg instanceof Long) {
+                                    Long data = (Long) msg;
+                                    System.out.println(data);
+                                }
+                            }
+                        });
+                    }
+                })
+                // 设置channel的参数
+                .childOption(ChannelOption.SO_KEEPALIVE, true).childOption(ChannelOption.TCP_NODELAY, true);
         ChannelFuture bind = serverBootstrap.bind("127.0.0.1", 8080);
         try {
             // 等待服务端监听端口关闭
+            System.out.println("netty server start。。"); // 打印启动信息
             bind.sync().addListener(FIRE_EXCEPTION_ON_FAILURE);
         } catch (InterruptedException e) {
             // 开启错误
@@ -67,38 +67,30 @@ public class ServerTest {
 
 }
 
-/**
- * 心跳检测
- */
-@Sharable
-class IdleTimeoutHandler extends ChannelDuplexHandler {
+class CustomChannelDeocder extends ByteToMessageDecoder {
 
+    /**
+     *
+     * @param ctx 上下文对象
+     * @param in  网络IO传输过来的数据，存储到ByteBuf类型的in中
+     * @param out 解码处理完后，把每一个"Message"都存储到out这一集合中，便于后续Netty遍历该out集合进行执行pipeline流水线的Handler操作
+     *            Netty是以消息Message为单位进行处理数据的，有多少Message，就执行多少次pipeline
+     *            ---->
+     *            如果字节数不超过滑动窗口，socket缓冲区，ByteBuf缓冲区，客户端只发一次数据即可，为什么服务端能划分出多个Message呢？
+     *            原因很简单：每调用一次decode进行处理ByteBuf，那么就会封装成一个Message。
+     *            如果服务端一次没有处理完ByteBuf，那么会进行第二次调用decode方法的处理，则第二次处理ByteBuf，则又会生成一个新的Message
+     *            以此类推，直到ByteBuf中的数据都处理完。
+     *            调用n次decode方法，处理ByteBuf n次，存储n个Message到out集合中
+     *            ---->
+     *            啥时候意味着ByteBuf中的数据都处理完？
+     *            其实还是指针的移动，思考一下之前NIO的各种指针，当读指针到达limit指针边界后就意味着ByteBuf处理完毕
+     * @throws Exception
+     */
     @Override
-    public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        System.out.println("连接被激活");
-        super.channelActive(ctx);
+    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+        System.out.println("收到自定义消息：");
+        long reciveLong = in.readLong();
+        out.add(reciveLong);
     }
 
-    @Override
-    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
-        if (evt instanceof IdleStateEvent) {
-            IdleState e = ((IdleStateEvent)evt).state();
-            // IdleState.READER_IDLE：表示在指定的时间间隔内没有读取到任何数据。
-            // IdleState.WRITER_IDLE：表示在指定的时间间隔内没有写入任何数据。
-            // IdleState.ALL_IDLE：表示在指定的时间间隔内既没有读取到任何数据，也没有写入任何数据。
-            if (e == IdleState.READER_IDLE) {
-                // 在调用close()时，未调用channelInactive()可能会导致一些问题。首先，如果未调用channelInactive()，
-                // 处理器可能无法及时处理Channel变为非活动状态的情况，从而导致资源泄漏或其他不可预料的问题。其次，
-                // 未调用channelInactive()可能会导致处理器无法正确地处理连接断开的情况，从而无法及时释放相关资源或执行必要的清理操作。
-                // 为了避免这些问题，建议在调用close()之前，先调用channelInactive()方法来通知处理器当前的Channel已经变为非活动状态。
-                // 这样可以确保处理器能够及时处理非活动状态，并执行必要的清理操作。
-                // 方法会触发ChannelInactive事件，执行下一个的ChannelInboundHandler的channelInactive方法
-                ctx.fireChannelInactive();
-                ctx.close();
-            }
-        } else {
-            // 调用父类的userEventTriggered方法
-            super.userEventTriggered(ctx, evt);
-        }
-    }
 }
